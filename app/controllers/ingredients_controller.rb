ff1ccc27704b94958fbc549ad4e53cdc8c1f7f74
derrick b <dbozich1@gmail.com>
2018-08-28 04:35:19 +0000

    automatically backed up by learn

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -2,4 +2,8 @@ class IngredientsController < ApplicationController
   def new
     @ingredient = Ingredient.new
   end
+
+  def edit
+    @ingredient = Ingredient.find_by_id(params[:id])
+  end
 end
