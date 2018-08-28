92ee3eac40ed690fdff231b736565e9243205f71
derrick b <dbozich1@gmail.com>
2018-08-28 05:05:47 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,4 +6,27 @@ class RecipesController < ApplicationController
   def edit
     @recipe = Recipe.find_by_id(params[:id])
   end
+
+
+
+  def update
+    @recipe = Recipe.find_by_id(params[:id])
+
+    @recipe.update(:name => params[:recipe][:name])
+    @recipe.save
+  end
+
+
+
+  def create
+    @recipe = Recipe.create(:name => params[:name])
+    redirect_to recipe_path(@recipe)
+  end
+
+  def show
+    binding.pry
+    @recipe = Recipe.find_by_id(params[:id])
+  end
+
+
 end
