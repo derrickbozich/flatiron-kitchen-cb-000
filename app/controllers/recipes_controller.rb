dc527bc16f2490ae784cbe899f245d18ef153668
derrick b <dbozich1@gmail.com>
2018-08-28 05:10:57 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -28,5 +28,11 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find_by_id(params[:id])
   end
 
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name)
+  end
+
 
 end
