129a32bffdefa5a4a5075ad1b2a46922435dc64e
derrick b <dbozich1@gmail.com>
2018-08-28 04:54:53 +0000

    automatically backed up by learn

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -13,4 +13,8 @@ class IngredientsController < ApplicationController
     @ingredient = Ingredient.create(:name => params[:name])
     redirect_to ingredient_path(@ingredient)
   end
+
+  def show
+    @ingredient = Ingredient.find_by_id(params[:id])
+  end
 end
